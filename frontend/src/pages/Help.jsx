172789ad19033f4a640b33b667d33d369e986b2d
chelly <164139757+raechelly7@users.noreply.github.com>
2026-03-12 17:02:25 +0800

    added email notification

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -1,7 +1,33 @@
+import FAQItem from '../components/FAQItem';
+
 export default function Help() {
   return (
-    <div className="p-5">
-      <h1 className="text-2xl font-bold text-gabay-blue">Help Page</h1>
+    <div className="max-w-5xl mx-auto p-10 animate-in fade-in duration-500">
+      <h1 className="text-4xl lg:text-6xl font-montserrat font-bold text-gabay-blue leading-tight text-center mb-4 mt-5 ">Help Center</h1>
+      <p className="font-poppins text-lg mt-6 mb-12 text-center text-gray-600">Here are some answers to common questions about reserving your appointment with GABAY.</p>
+      
+      <div className="bg-white rounded-2xl shadow-sm border border-gray-300 overflow-hidden">
+        <FAQItem 
+          question="How do I book an appointment?" 
+          answer="Navigate to the Departments page, select either General or Specialty, fill out the form, and wait for the confirmation in your Inbox." 
+        />
+        <FAQItem 
+          question="What is the difference between the General and Specialty Departments?" 
+          answer="General consultations are for common illnesses. Specialty consultations are for treating specific conditions." 
+        />
+        <FAQItem 
+          question="What is a Medical Referral?" 
+          answer="A document from a doctor referring you to a specialist. This is a required attachment for Specialty appointments." 
+        />
+        <FAQItem 
+          question="What if I don't have a Hospital Number?" 
+          answer="GABAY can generate you a hospital number if you are a new patient. Please take note of it since it will be PERMANENT." 
+        />
+        <FAQItem 
+          question="Can I book for someone else?" 
+          answer="Appointment reservations are tied to the patient's Hospital Number. To maintain accurate records, each patient should have their own account." 
+        />
+      </div>
     </div>
   );
 }
