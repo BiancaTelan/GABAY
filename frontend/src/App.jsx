1353bfbfda39f193d1cec601ada1c4a086408ad0
chelly <164139757+raechelly7@users.noreply.github.com>
2026-04-08 14:17:49 +0800

    added admin route note

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -259,7 +259,7 @@ function App() {
             </Route>
 
           {/* ADMIN ROUTES */}
-          <Route element={<AdminRoute />}>
+          <Route element={<AdminRoute />}> {/* DONT REMOVE element={<AdminRoute><AdminLayout /></AdminRoute>}>*/}
             <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<AdminDashboard />} />
               <Route path="users" element={<Users />} />
