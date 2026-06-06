fe2311a12de801e6e0684819934f5834197bfef5
chelly <164139757+raechelly7@users.noreply.github.com>
2026-06-06 12:24:10 +0800

    added middle & extension name fields for patient

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -13,7 +13,9 @@ export default function Account({ userInfo, onLogout, onUpdateProfile }) {
   
   const [localUserInfo, setLocalUserInfo] = useState({
     firstname: "",
+    middlename: "",
     surname: "",
+    extension: "",
     hospital_num: "",
     email: "",
     contactNumber: "",
@@ -138,12 +140,12 @@ export default function Account({ userInfo, onLogout, onUpdateProfile }) {
 
     if (!localUserInfo.firstname.trim()) newErrors.firstname = "First name is required";
     else if (!namePattern.test(localUserInfo.firstname)) newErrors.firstname = "Name cannot contain numbers";
+    
+    if (!localUserInfo.middlename.trim()) newErrors.middlename = "Middle Name is required";
+    else if (!namePattern.test(localUserInfo.middlename)) newErrors.middlename = "Name cannot contain numbers";
 
     if (!localUserInfo.surname.trim()) newErrors.surname = "Last name is required";
     else if (!namePattern.test(localUserInfo.surname)) newErrors.surname = "Name cannot contain numbers";
-
-    if (!localUserInfo.email.trim()) newErrors.email = "Email address is required";
-    else if (!emailPattern.test(localUserInfo.email)) newErrors.email = "Enter a valid email address";
 
     if (!localUserInfo.dob.trim() || localUserInfo.dob === "MM/DD/YYYY") {
       newErrors.dob = "Date of birth is required";
@@ -257,13 +259,30 @@ const handleSave = async () => {
               {isEditing ? (
                 <>
                   <Input label="First Name" name="firstname" value={localUserInfo.firstname} onChange={handleInputChange} error={errors.firstname} isEditing={isEditing} required />
+                  <Input label="Middle Name" name="middlename" value={localUserInfo.middlename} onChange={handleInputChange} error={errors.middlename} isEditing={isEditing} />
                   <Input label="Last Name" name="surname" value={localUserInfo.surname} onChange={handleInputChange} error={errors.surname} isEditing={isEditing} required />
+                  <div className="flex flex-col gap-1 w-full">
+                  <label className="text-sm font-poppins font-medium text-gray-700">Name Extension</label>
+                  <select value={localUserInfo.extension || ""} onChange={(e) => setLocalUserInfo({ ...localUserInfo, extension: e.target.value })}
+                  className="w-full p-2 border border-gray-300 rounded-md font-poppins text-sm bg-white outline-none focus:ring-1 focus:ring-gabay-teal text-gray-700 cursor-pointer">
+                    <option value="">None (N/A)</option>
+                    <option value="Jr.">Jr.</option>
+                    <option value="Sr.">Sr.</option>
+                    <option value="III">III</option>
+                    <option value="III">IV</option>
+                    <option value="III">V</option>
+                  </select>
+                  </div>
                 </>
               ) : (
-                <Input label="Full Name" value={`${localUserInfo.firstname} ${localUserInfo.surname}`} readOnly noHover />
+                <Input label="Full Name" value={[
+                  localUserInfo.firstname,
+                  localUserInfo.middlename,
+                  localUserInfo.surname,
+                  localUserInfo.extension
+                  ].filter(Boolean).join('')} readOnly noHover />
               )}
               
-
               <Input label="Hospital Number" value={localUserInfo.hospital_num} readOnly noHover />
               <div className="relative">
                 <Input 
