7fcacc17346b995ec853b65634cab5102875a3ac
Maria Turano <turano.educ@gmail.com>
2026-06-08 09:05:47 +0800

    ;;;

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ const PatientRoute = () => {
   const location = useLocation();
   const isLoggedIn = !!token;
   
-  if (!isLoggedIn || (userRole !== 'patient')) {
+  if (!isLoggedIn || (userRole !== 'Patient')) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
   return <Outlet />; 
